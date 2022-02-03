9458b0fb001c550bbd9345577378708f0b052ca9
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-02-03 13:33:06 -0800

    all files added

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+import useStyles from './styles';
+
+function Post() {
+ const classes = useStyles();
+  return (
+
+      <h1>Post</h1>
+   
+  )
+}
+
+export default Post
