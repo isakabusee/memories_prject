d81d24e757ce1f6d1b6efa6aa13b87fee1809a79
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-02-07 15:56:04 -0800

    more progress

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -2,15 +2,23 @@ import React from 'react'
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import DeleteIcon from '@mui/icons-material/Delete';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import moment from 'moment'
 
 
 import useStyles from './styles';
 
-function Post() {
+function Post({ post }) {
  const classes = useStyles();
   return (
+        <Card className='{classes.card'>
+          <CardMedia className={classes.media} image={Post.selectedFile} title={post.title} />
+          <div className={classes.overlay}>
+            <Typography variant="h6">{post.creator}</Typography>
+            <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+          </div>
 
-      <h1>Post</h1>
+        </Card>
    
   )
 }
