2e705e2ae4829abcf5c2fe697f72d3f30deed076
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-02-09 00:00:05 -0800

    icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -17,6 +17,23 @@ function Post({ post }) {
             <Typography variant="h6">{post.creator}</Typography>
             <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
           </div>
+          <div className={classes.overlay2}>
+            <button style={{color: 'white'}} size="small" onClick={() => {}}>
+              <MoreHorizIcon fontSize="default" />
+            </button>
+          </div>
+          <div className={classes.details}>
+              <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
+          </div >
+          <CardContent>
+          <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+          </CardContent>
+          <CardActions className={classes.cardActions}>
+            <Button size="small" color="primary onClick={() => }">
+              <ThumbUpAltIcon fontSize="small" />
+            </Button>
+
+          </CardActions>
 
         </Card>
    
