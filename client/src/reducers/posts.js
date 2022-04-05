c3af6922110b149a3ba5d1e76e095c85dd734695
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-04-05 08:25:13 -0700

    need some debugging

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,17 +4,17 @@ export default (posts = [], action) => {
     switch(action.type) {
         case FETCH_ALL:
             return action.payload;
-        case DELETE:
-            return posts.filter((post) => post.id !== action.payload);
-
+            
         case UPDATE:
             return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
         case LIKE:
-            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+                    
         case CREATE:
             return [...posts, action.payload];
-        default:
+        case DELETE:
+            return posts.filter((post) => post.id !== action.payload);
+           default:
             return posts;
     }
-}
+};
